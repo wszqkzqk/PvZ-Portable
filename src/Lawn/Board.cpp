ecc515f0171600601cdd233be21ee971b94a6f69
peashooter2 <112814689+peashooter2@users.noreply.github.com>
2026-07-05 08:36:04 +0800

    Fix: Correct the mRiseFromGraveCounter constant in Board::SpawnZombieWave from 210 to 200

--- src/Lawn/Board.cpp
+++ src/Lawn/Board.cpp
@@ -5067,7 +5067,7 @@ void Board::SpawnZombieWave()
 
 	if (mCurrentWave == mNumWaves - 1 && !mApp->IsContinuousChallenge())
 	{
-		mRiseFromGraveCounter = 210;
+		mRiseFromGraveCounter = 200;
 	}
 	if (IsFlagWave(mCurrentWave))
 	{
