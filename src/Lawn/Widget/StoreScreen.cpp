90704af6919d918b625c1fb5c3edbfd66b4b442f
Zhou Qiankang <wszqkzqk@qq.com>
2026-07-13 15:02:04 +0800

    Fix: restore Tree of Wisdom challenge record write on purchase with correct index 49
    Signed-off-by: Zhou Qiankang <wszqkzqk@qq.com>

--- src/Lawn/Widget/StoreScreen.cpp
+++ src/Lawn/Widget/StoreScreen.cpp
@@ -1011,6 +1011,7 @@ void StoreScreen::PurchaseItem(StoreItem theStoreItem)
             else if (theStoreItem == STORE_ITEM_TREE_OF_WISDOM)
             {
                 mApp->mPlayerInfo->mPurchases[theStoreItem] = 1;
+                mApp->mPlayerInfo->mChallengeRecords[GAMEMODE_TREE_OF_WISDOM - GAMEMODE_SURVIVAL_NORMAL_STAGE_1] = 1;
 
                 LawnDialog* aDialog = (LawnDialog*)mApp->DoDialog(
                     DIALOG_STORE_PURCHASE, 
