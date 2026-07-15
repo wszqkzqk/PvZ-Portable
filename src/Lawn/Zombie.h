f6582f55a31872c23702e6ebd9151d6e2a779aae
Zhou Qiankang <wszqkzqk@qq.com>
2026-07-15 09:47:22 +0800

    Fix: correct mind-controlled zombie tint color to match original
    Signed-off-by: Zhou Qiankang <wszqkzqk@qq.com>

--- src/Lawn/Zombie.h
+++ src/Lawn/Zombie.h
@@ -55,7 +55,7 @@ constexpr const float THOWN_ZOMBIE_GRAVITY = 0.05f;
 constexpr const float CHILLED_SPEED_FACTOR = 0.4f;
 constexpr const float CLIP_HEIGHT_LIMIT = -100.0f;
 constexpr const float CLIP_HEIGHT_OFF = -200.0f;
-const Color ZOMBIE_MINDCONTROLLED_COLOR = Color(128, 0, 192, 255);
+constexpr Color ZOMBIE_MINDCONTROLLED_COLOR = Color(128, 64, 192, 255);
 
 enum ZombieAttackType : int32_t
 {
